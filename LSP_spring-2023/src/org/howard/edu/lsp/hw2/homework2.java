c89587d693f931995366122c0b35d6836d4d884e
barry <barry@10.0.0.107>
2023-02-06 23:21:41 -0500

    Homework number 2

---
+++ LSP_spring-2023/src/org/howard/edu/lsp/hw2/homework2.java
@@ -0,0 +1,32 @@
+package org.howard.edu.lsp.hw2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class homework2 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		
+		Scanner txtFile = new Scanner(new File("C:\\Users\\barry\\git\\LSP_assignment1BCC\\LSP_spring-2023\\test.txt"));
+		while (txtFile.hasNext ()) {
+			String word = txtFile.next();
+			if (map.containsKey(word)) {
+				int count = map.get(word) +1;
+				map.put(word,  count);
+			}
+			else {
+				map.put(word,  1);
+			}
+		}
+		txtFile.close();
+		for (Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println(entry);
+		}
+	}
+
+}
